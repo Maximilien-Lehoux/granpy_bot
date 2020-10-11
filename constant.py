da10fdfeefc7a4385a875b61ce1437c5ae868e1c
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2020-10-11 16:22:57 +0200

    envoyé le fichier constant

---
+++ constant.py
@@ -0,0 +1,7 @@
+"""you can modify API keys with your own for professional use"""
+
+KEY_API_GEOCODE = "AIzaSyA9LIoaH1wgEHf0y_fIq7q8ikD5_tgjO80"
+
+KEY_GOOGLE_MAP = "AIzaSyBC-8eKYcBYOqthRdC9FcikffcNB1HkJvU"
+
+URL_WIKIPEDIA = "https://fr.wikipedia.org/w/api.php"
